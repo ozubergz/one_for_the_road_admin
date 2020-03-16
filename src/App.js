da4e1714f64d590d857c53a1babbcee6cb856aca
ozubergz <jmin499@gmail.com>
2020-03-16 19:48:07 -0400

    admin dashboard

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import React, { Component } from 'react';
 import './styles/App.css';
 import simpleRestProvider from 'ra-data-simple-rest';
-import { 
-  Admin, 
-  Resource
-} from 'react-admin';
+import { Admin, Resource } from 'react-admin';
 import { UserList, UserShow } from './Users/index';
 import { OrderList, OrderShow } from './Orders/index';
 import { ItemList, ItemEdit, ItemCreate } from './Items/index';
 import { CategoryList, CategoryEdit, CategoryCreate } from './Categories/index';
+import Dashboard from './Dashboard';
 import actionCable from 'actioncable';
+import ReactNotification from 'react-notifications-component';
+import 'react-notifications-component/dist/theme.css';
+import { store } from 'react-notifications-component';
 
-const dataProvider = simpleRestProvider('http://localhost:3000/api');
 
 const CableApp = {};
 CableApp.cable = actionCable.createConsumer("ws://localhost:3000/cable");
 
+const dataProvider = simpleRestProvider('http://localhost:3000/api');
+
 class App extends Component {
   
-
   componentDidMount() {
     CableApp.cable.subscriptions.create({
         channel: 'NotificationChannel'
       }, {
         received: (data) => {
+          //receive notification when client makes an order
           console.log(data)
         }
       }
@@ -32,8 +34,8 @@ class App extends Component {
   
   render() {
     return (
-        <div className="App">
-        <Admin dataProvider={dataProvider}>
+      <div className="App">
+        <Admin dashboard={Dashboard} dataProvider={dataProvider}>
           <Resource name="categories" list={CategoryList} edit={CategoryEdit} create={CategoryCreate} />
           <Resource name="items" list={ItemList} edit={ItemEdit} create={ItemCreate} />
           <Resource name="users" list={UserList}  show={UserShow} />
