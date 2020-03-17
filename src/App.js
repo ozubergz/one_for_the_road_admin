5c1ca821e2910522272f853e143e7064c7c9fb3c
ozubergz <jmin499@gmail.com>
2020-03-16 22:56:28 -0400

    notification fixed bugs

--- src/App.js
+++ src/App.js
@@ -12,21 +12,75 @@ import Dashboard from './Dashboard';
 
 //React Notification Components
 import ReactNotification from 'react-notifications-component';
+import 'react-notifications-component/dist/theme.css';
+import { store } from 'react-notifications-component';
+
+//Action Cable
+import actionCable from 'actioncable';
+
+//audio
+import bellAudio from './sounds/bell.mp3';
+import UIfx from 'uifx';
+
+const bell = new UIfx(bellAudio, {
+    volume: 0.4,
+    throttleMs: 100
+});
+
+//connection to Action Cable
+const CableApp = {};
+CableApp.cable = actionCable.createConsumer("ws://localhost:3000/cable");
 
 const dataProvider = simpleRestProvider('http://localhost:3000/api');
 
-const App = () => {  
-  return (
-    <div className="App">
-      <ReactNotification />
-      <Admin dashboard={Dashboard} dataProvider={dataProvider}>
-        <Resource name="categories" list={CategoryList} edit={CategoryEdit} create={CategoryCreate} />
-        <Resource name="items" list={ItemList} edit={ItemEdit} create={ItemCreate} />
-        <Resource name="users" list={UserList}  show={UserShow} />
-        <Resource name="orders" list={OrderList} show={OrderShow} />
-      </Admin>
-    </div>
-  );
+class App extends Component {  
+
+  componentDidMount() {
+    CableApp.cable.subscriptions.create({
+        channel: 'NotificationChannel'
+    }, {
+        received: (message) => {
+            //receive notification when client makes an order
+            this.handleNotification(message);
+            bell.play();
+        }
+    });
+  }
+
+  handleNotification(message) {
+    store.addNotification({
+        title: "Success!",
+        message: message,
+        type: "success",
+        insert: "top",
+        container: "top-right",
+        animationIn: ["animated", "fadeIn"],
+        animationOut: ["animated", "fadeOut"],
+        dismiss: {
+            duration: 5000,
+            onScreen: true
+        },
+        slidingEnter: {
+            duration: 300,
+            timingFunction: 'ease-in',
+            delay: 0
+        }
+    });
+  }
+  
+  render() {
+    return (
+        <div className="App">
+        <ReactNotification />
+        <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+          <Resource name="categories" list={CategoryList} edit={CategoryEdit} create={CategoryCreate} />
+          <Resource name="items" list={ItemList} edit={ItemEdit} create={ItemCreate} />
+          <Resource name="users" list={UserList}  show={UserShow} />
+          <Resource name="orders" list={OrderList} show={OrderShow} />
+        </Admin>
+      </div>
+    );
+  }
 }
 
 export default App;
