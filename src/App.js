ff3ba1cd7f53a41b0880ff63ab06d9483d5c71c1
ozubergz <jmin499@gmail.com>
2020-03-16 20:20:39 -0400

    react notification shows

--- src/App.js
+++ src/App.js
@@ -1,36 +1,42 @@
 import React, { Component } from 'react';
 import './styles/App.css';
 import simpleRestProvider from 'ra-data-simple-rest';
+
+//React Admin Components
 import { Admin, Resource } from 'react-admin';
 import { UserList, UserShow } from './Users/index';
 import { OrderList, OrderShow } from './Orders/index';
 import { ItemList, ItemEdit, ItemCreate } from './Items/index';
 import { CategoryList, CategoryEdit, CategoryCreate } from './Categories/index';
 import Dashboard from './Dashboard';
+
+//Action Cable
 import actionCable from 'actioncable';
+
+//React Notification Components
 import ReactNotification from 'react-notifications-component';
 import 'react-notifications-component/dist/theme.css';
 import { store } from 'react-notifications-component';
 
 
-const CableApp = {};
-CableApp.cable = actionCable.createConsumer("ws://localhost:3000/cable");
+// const CableApp = {};
+// CableApp.cable = actionCable.createConsumer("ws://localhost:3000/cable");
 
 const dataProvider = simpleRestProvider('http://localhost:3000/api');
 
 class App extends Component {
   
-  componentDidMount() {
-    CableApp.cable.subscriptions.create({
-        channel: 'NotificationChannel'
-      }, {
-        received: (data) => {
-          //receive notification when client makes an order
-          console.log(data)
-        }
-      }
-    )
-  }
+  // componentDidMount() {
+  //   CableApp.cable.subscriptions.create({
+  //       channel: 'NotificationChannel'
+  //     }, {
+  //       received: (data) => {
+  //         //receive notification when client makes an order
+  //         console.log(data)
+  //       }
+  //     }
+  //   )
+  // }
   
   render() {
     return (
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 
+//React Notification Components
+import ReactNotification from 'react-notifications-component';
+import 'react-notifications-component/dist/theme.css';
+import { store } from 'react-notifications-component';
+// import actionCable from 'actioncable';
+
+
 class Dashboard extends Component {
     render() {
         return (
             <div>
+                <ReactNotification />
                 <h1>Welcome to the administration page</h1>
+                <button
+                    onClick={() => {
+                        store.addNotification({
+                            title: 'Dropbox',
+                            message: 'Files were synced',
+                            type: 'default',                         // 'default', 'success', 'info', 'warning'
+                            container: 'top-right',                // where to position the notifications
+                            animationIn: ["animated", "fadeIn"],     // animate.css classes that's applied
+                            animationOut: ["animated", "fadeOut"],   // animate.css classes that's applied
+                            dismiss: {
+                                duration: 3000 
+                            }
+                        })
+                    }}
+                >
+                    Add notification
+                </button>
             </div>
         );
     }
