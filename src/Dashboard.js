cf0594b16db20306c1f6e00edbc1bda8b556cee5
ozubergz <jmin499@gmail.com>
2020-05-16 14:41:33 -0400

    update dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 
+const ROOT_URL = "http://localhost:3000/api/"
+
 class Dashboard extends Component {
+    
+    state = {
+        orders: []
+    }
+    
+    componentDidMount() {
+        fetch(`${ROOT_URL}orders`)
+        .then(res => res.json())
+        .then(orders => {
+            this.setState({ orders })
+        })
+    }
+    
     render() {
         return (
             <div>
                 <h1>One for the Road Admin</h1>
+                
             </div>
         );
     }
