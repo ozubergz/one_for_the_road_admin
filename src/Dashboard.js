50408863d0e0e3e9365b9258a113d6c662a6d224
ozubergz <jmin499@gmail.com>
2020-05-16 15:05:11 -0400

    update dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,4 +1,12 @@
 import React, { Component } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+// import Divider from '@material-ui/core/Divider';
+import ListItemText from '@material-ui/core/ListItemText';
+// import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+// import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
 
 const ROOT_URL = "http://localhost:3000/api/"
 
@@ -15,12 +23,31 @@ class Dashboard extends Component {
             this.setState({ orders })
         })
     }
+
+    renderListItems() {
+        return ( 
+            this.state.orders.map(order => {
+                return (
+                    <ListItem key={order.id}>
+                        <ListItemText 
+                            primary={order.customer}
+                            
+                        />
+                    </ListItem>
+                )
+            })
+        )
+    }
     
     render() {
         return (
             <div>
                 <h1>One for the Road Admin</h1>
                 
+                <List>
+                    {this.renderListItems()}
+                </List>
+
             </div>
         );
     }
